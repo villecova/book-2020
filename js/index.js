b04002dac991e4c7eb33d3f27f46e706ed56e9aa
villecova <75551900+villecova@users.noreply.github.com>
2021-06-27 16:01:44 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// $(document).ready(function() {
+
+// 	var typed = new Typed(".element", {
+// 	strings: ["a Graphic Designer", "a Web Designer", "a Web Developer"],
+// 	smartBackspace: true;
+// 	typeSpeed: 100,
+// 	backSpeed: 100,
+// 	loop: true,
+// 	loopCount: Infinity,
+// 	startDelay: 1000,
+// 	});
+
+// });
+
+var typed = new Typed('.element', {
+  strings: ["Graphic Designer.", "Web Designer.", "Web Developer."],
+  typeSpeed: 120,
+  backSpeed: 50,
+  loop: true,
+  loopCount: Infinity
+});
