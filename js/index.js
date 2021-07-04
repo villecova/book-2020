87018b9d704c6445eb409fd5cc4ee1d2a8e3d52f
villecova <75551900+villecova@users.noreply.github.com>
2021-07-04 14:19:28 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -34,7 +34,9 @@
   // TYPED EFFECT & FOCUS HOME SECTION ----------------------------------------------------
 
   //.includes
-  if (document.URL.includes("portfolio" || "index.html")) {
+  //https://villecova.github.io/portfolio/
+  //if (document.URL.includes("portfolio" || "index.html")) {
+  if (document.URL.includes("https://villecova.github.io/portfolio/" || "index.html")) {
 
     document.getElementById('home-section').focus();
 
